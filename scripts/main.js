e2b378a2a11f4e05a5f01223c933ad36a561df6d
stonefarmer9 <g.p.levaillant@gmail.com>
2019-01-22 16:39:08 +0000

    Events can be posted to array

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+let whatsOn = new EventList("WhatsOn")
 
+var SubmitEvent = document.getElementById("SubmitEvent")
 
-function postEvent() {
+function openInput() {
 var x = document.getElementById("PostEvent");
 if (x.style.display === "none") {
 x.style.display = "block";
@@ -8,3 +10,15 @@ x.style.display = "block";
 x.style.display = "none";
 }
 }
+
+function postEvent() {
+  var name = document.getElementById("eventnamepost").value
+  var date = document.getElementById("datepost").value
+  whatsOn.addEvent(name, date)
+}
+
+SubmitEvent.onclick = function() {
+  postEvent()
+  openInput()
+  console.log(whatsOn.list)
+};
